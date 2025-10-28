7502495d6879cad61b11f4826f77e520e5327985
Bruno <brunocostascuciato009@gmail.com>
2025-10-28 08:44:01 -0300

    chore: Criando projeto cardapio

---
+++ src/data/dados.js
@@ -0,0 +1,56 @@
+const menu = [
+
+    {
+        id: 1,
+        titulo: "Jogo 1",
+        categoria: "Ação",
+        preco: 200,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 1"
+    },
+
+    {
+        id: 2,
+        titulo: "Jogo 2",
+        categoria: "Ação",
+        preco: 200,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 2"
+    },
+    {
+        id: 3,
+        titulo: "Jogo 3",
+        categoria: "Ação",
+        preco: 200,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 3"
+    },
+
+    {
+        id: 4,
+        titulo: "Jogo 4",
+        categoria: "Terror",
+        preco: 200,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 4"
+    },
+
+    {
+        id: 5,
+        titulo: "Jogo 5",
+        categoria: "Terror",
+        preco: 200,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 5"
+    },  
+
+    {
+        id: 6,
+        titulo: "Jogo 6",
+        categoria: "Esportes",
+        preco: 280,
+        imagem: "/src/assets/img.jpg",
+        descricao: "Descrição do Jogo 6"    
+    }   
+];
+export default menu;
