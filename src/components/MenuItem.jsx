46b114dc48b1785f0f01afb432cc3c4557deb243
Bruno <brunocostascuciato009@gmail.com>
2025-10-28 09:51:26 -0300

    style: estiliando o projeto com o tailwindcss

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,16 +1,48 @@
-const MenuItem = ({titulo,preco,imagem,descricao}) => {
+import { useState } from "react"
+import Menu from  './components/Menu'
+import Categorias from  './components/Categorias'
+import BD from './data/dados'
 
-    return (
-        <article>
-            <imgagem src={imagem} alt={titulo} />
-            <div>
-                <header>
-                    <h3>{titulo}</h3>
-                    <p>{preco.toFixed(2)}</p>
-                </header>
-                <p>{descricao}</p>
-            </div>
-        </article>
-    )
+// OBTÉM TODAS AS CATEGORIAS UNICAS DO MENU, ADICIONADO "TODOS " NO INICIO DA PÁGINA
+const todasCategorias=["todos",...new Set(BD.map((item)=>item.opcao))]
+
+function App() {
+ 
+  //HOOK: useState- manipula o estado da variavel
+  const [menuItems,setMenuItems]= useState(BD);
+  const [categorias] = useState(todasCategorias);
+
+  //FUNÇÃO FILTRAR ITEMS
+
+  const filtrarItems =(opcao)=>{
+    if(opcao ==="todos"){
+      setMenuItems(BD); // mostra todos os items
+    }else {
+      const novoItem = BD.filter((item)=>item.opcao ===opcao);
+      setMenuItems(novoItem); // mostra apenas o item filtrado
+    }
+    
+  }
+
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>Cardápio de Games</h1>
+          <div></div>
+        </div>
+      </header>
+     <main>
+      <section>
+        <Categorias categorias={categorias} filtroItems={filtrarItems}/>
+        <Menu items={menuItems}/>
+      </section>
+     </main>
+     <foote>
+        <p>&copy; 2025 todos direitos reservados.</p>
+     </foote>
+    </div>
+  )
 }
-export default MenuItem;
+
+export default App
